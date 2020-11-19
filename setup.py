da5440d49131e181633c8c2d16b952f8e8324a6d
Dan Watson <dcwatson@gmail.com>
2020-11-19 10:33:28 -0500

    Inherit PATH when building

--- setup.py
+++ setup.py
@@ -14,6 +14,13 @@ with open(os.path.join(BASE_DIR, "README.md"), "r") as readme:
     long_description = readme.read()
 
 
+def env(**kwargs):
+    base = {"PATH": os.environ["PATH"]}
+    for name, value in kwargs.items():
+        base[name.upper()] = value
+    return base
+
+
 class DeflateBuilder(build_ext):
     def run(self, *args, **kwargs):
         build_dir = os.path.join(BASE_DIR, "build")
@@ -30,7 +37,7 @@ class DeflateBuilder(build_ext):
                 lib_name = os.path.join(build_dir, "libdeflate.{}.a".format(arch))
                 cflags = "-fPIC -arch {} -mmacosx-version-min={}".format(arch, target)
                 # -B tells make to always rebuild, so we don't need to clean between builds.
-                subprocess.run(["make", "clean", "libdeflate.a"], cwd=libdeflate_dir, env={"CFLAGS": cflags})
+                subprocess.run(["make", "clean", "libdeflate.a"], cwd=libdeflate_dir, env=env(cflags=cflags))
                 subprocess.run(["mv", "-f", "libdeflate.a", lib_name], cwd=libdeflate_dir)
                 lipo_libs.append(lib_name)
             # Join all the architectures into a single universal library.
@@ -38,14 +45,16 @@ class DeflateBuilder(build_ext):
         else:
             # Only need to build the static library we're going to link, no need for programs.
             # Even though we build libdeflate as static, we might be built as shared, so make sure libdeflate is PIC.
-            result = subprocess.run(["make", "clean", "libdeflate.a"], cwd=libdeflate_dir, env={"CFLAGS": "-fPIC"})
+            result = subprocess.run(["make", "clean", "libdeflate.a"], cwd=libdeflate_dir, env=env(cflags="-fPIC"))
         if result.returncode == 0:
             super().run(*args, **kwargs)
+            """"
             if is_apple_silicon and not APPLE_UNIVERSAL:
                 # Our extension will likely have been built universal, so if we didn't also build libdeflate universal,
                 # thin out the resulting library to just arm64.
                 for lib in self.get_outputs():
                     subprocess.run(["lipo", lib, "-thin", "arm64", "-output", lib])
+            """
         else:
             print("*** Failed to build libdeflate")
 
