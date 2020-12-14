299d7d74f79dc50a89770f61f42969abc836a158
Dan Watson <dcwatson@gmail.com>
2020-12-14 14:12:33 -0500

    Stop trying to link libdeflate and just build it

--- setup.py
+++ setup.py
@@ -1,88 +1,30 @@
 import os
-import platform
-import subprocess
-import sysconfig
-from distutils.command.build_ext import build_ext
 
 from setuptools import Extension, setup
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-APPLE_SILICON = platform.system() == "Darwin" and platform.machine() == "arm64"
-APPLE_UNIVERSAL = os.getenv("APPLE_UNIVERSAL", "0") == "1"
-WINDOWS = platform.system() == "Windows"
-
-STATIC_LIB = "libdeflatestatic.lib" if WINDOWS else "libdeflate.a"
-
+LIBDEFLATE_DIR = os.path.join(BASE_DIR, "libdeflate")
 
 with open(os.path.join(BASE_DIR, "README.md"), "r") as readme:
     long_description = readme.read()
 
 
-def env(**kwargs):
-    base = {"PATH": os.environ["PATH"]}
-    for name, value in kwargs.items():
-        base[name.upper()] = value
-    return base
-
-
-class DeflateBuilder(build_ext):
-    def run(self, *args, **kwargs):
-        build_dir = os.path.join(BASE_DIR, "build")
-        libdeflate_dir = os.path.join(BASE_DIR, "libdeflate")
-        # Building a universal library (at least right now) means building libdeflate twice and joining them.
-        if APPLE_SILICON and APPLE_UNIVERSAL:
-            lipo_libs = []
-            # Make sure we target the same version of macOS Python did, so the linker doesn't complain.
-            target = sysconfig.get_config_var("MACOSX_DEPLOYMENT_TARGET")
-            os.makedirs(build_dir, exist_ok=True)
-            for arch in ("arm64", "x86_64"):
-                lib_name = os.path.join(build_dir, "libdeflate.{}.a".format(arch))
-                cflags = "-fPIC -arch {} -mmacosx-version-min={}".format(arch, target)
-                # -B tells make to always rebuild, so we don't need to clean between builds.
-                subprocess.run(
-                    ["make", "clean", STATIC_LIB],
-                    cwd=libdeflate_dir,
-                    env=env(cflags=cflags),
-                )
-                subprocess.run(["mv", "-f", STATIC_LIB, lib_name], cwd=libdeflate_dir)
-                lipo_libs.append(lib_name)
-            # Join all the architectures into a single universal library.
-            result = subprocess.run(
-                ["lipo", "-create", *lipo_libs, "-output", STATIC_LIB],
-                cwd=libdeflate_dir,
-            )
-        elif WINDOWS:
-            result = subprocess.run(
-                ["nmake", "/f", "Makefile.msc", "clean", STATIC_LIB],
-                cwd=libdeflate_dir,
-                shell=True,
-            )
-        else:
-            # Only need to build the static library we're going to link, no need for programs.
-            # Even though we build libdeflate as static, we might be built as shared, so make sure libdeflate is PIC.
-            result = subprocess.run(
-                ["make", "clean", STATIC_LIB],
-                cwd=libdeflate_dir,
-                env=env(cflags="-fPIC"),
-            )
-        if result.returncode == 0:
-            super().run(*args, **kwargs)
-            """"
-            if is_apple_silicon and not APPLE_UNIVERSAL:
-                # Our extension will likely have been built universal, so if we didn't also build libdeflate universal,
-                # thin out the resulting library to just arm64.
-                for lib in self.get_outputs():
-                    subprocess.run(["lipo", lib, "-thin", "arm64", "-output", lib])
-            """
-        else:
-            print("*** Failed to build libdeflate")
-
-
 deflate = Extension(
     "deflate",
-    sources=["deflate.c"],
-    extra_objects=["libdeflate/{}".format(STATIC_LIB)],
+    sources=[
+        "libdeflate/lib/adler32.c",
+        "libdeflate/lib/crc32.c",
+        "libdeflate/lib/deflate_compress.c",
+        "libdeflate/lib/deflate_decompress.c",
+        "libdeflate/lib/gzip_compress.c",
+        "libdeflate/lib/gzip_decompress.c",
+        "libdeflate/lib/utils.c",
+        "libdeflate/lib/zlib_compress.c",
+        "libdeflate/lib/zlib_decompress.c",
+        "libdeflate/lib/x86/cpu_features.c",
+        "deflate.c",
+    ],
+    include_dirs=[LIBDEFLATE_DIR],
 )
 
 setup(
@@ -95,6 +37,5 @@ setup(
     author_email="dcwatson@gmail.com",
     url="https://github.com/dcwatson/deflate",
     license="MIT",
-    cmdclass={"build_ext": DeflateBuilder},
     ext_modules=[deflate],
 )
