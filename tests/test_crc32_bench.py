b3698d4e1fdca2b5f691521ca9af08613f2d955d
Thomas Waldmann <tw@waldmann-edv.de>
2022-02-28 21:05:53 +0100

    benchmark deflate.crc32 against zlib.crc32

---
+++ tests/test_crc32_bench.py
@@ -0,0 +1,20 @@
+# benchmark deflate crc32 against zlib crc32
+# note: we have other tests for correctness of the result, so we do not check it here.
+
+import deflate
+import os
+import zlib
+
+data = os.urandom(10 * 1000000)
+
+
+def test_deflate_crc32(benchmark):
+    @benchmark
+    def result():
+        deflate.crc32(data)
+
+
+def test_zlib_crc32(benchmark):
+    @benchmark
+    def result():
+        zlib.crc32(data)
